ff7af82b4d574dc74f553a9474b8fde48ad86371
sofialundkvist <sofialundkvist@outlook.com>
2018-06-27 17:11:08 +0200

    describe resources on start page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center align-items-stretch">
         <div class="col-md-12 mb-4">
             <div class="card">
                 <div class="card-header">
@@ -15,7 +15,37 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    Här kan du skapa veckoplaner med recept som du vill laga.
+                    Här kan du spara recept och kategorisera dem samt skapa veckoplaner med recept att laga.
+                </div>
+            </div>
+        </div>
+         <div class="col-md-4 mb-4">
+            <div class="card">
+                <div class="card-header">
+                    <h2>Recept</h2>
+                </div>
+                <div class="card-body">
+                    Du kan lägga till recept med ingridienser och instruktioner. Du kan även se andra användares recept.
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <div class="card-header">
+                    <h2>Veckoplaner</h2>
+                </div>
+                <div class="card-body">
+                    Du kan skapa veckoplaner och lägga till recept som du vill laga i dem.
+                </div>
+            </div>
+        </div>
+         <div class="col-md-4 mb-4">
+            <div class="card">
+                <div class="card-header">
+                    <h2>Kategorier</h2>
+                </div>
+                <div class="card-body">
+                    Du kan skapa kategorier och lägga till recept i dessa.
                 </div>
             </div>
         </div>
